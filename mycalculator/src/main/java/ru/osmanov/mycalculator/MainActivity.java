c7e1d147aed19906072bd0fca33467dc2d372a45
Nurulla Osmanov <Shivaii@yandex.ru>
2022-12-20 14:53:23 +0300

    сверстал все кнопки для калькулятора

--- mycalculator/src/main/java/ru/osmanov/mycalculator/MainActivity.java
+++ mycalculator/src/main/java/ru/osmanov/mycalculator/MainActivity.java
@@ -10,34 +10,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Integer num1;
-    private Integer num2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.button4);
-        final EditText et1 = findViewById(R.id.editText1);
-        final EditText et2 = findViewById(R.id.editText2);
-        TextView textView = findViewById(R.id.textView);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    num1 = Integer.valueOf(et1.getText().toString());
-                    num2 = Integer.valueOf(et2.getText().toString());
-                    if (num1.equals(num2)) {
-                        textView.setText("Равно!");
-                    } else {
-                        textView.setText("Не равно!");
-                    }
-                } catch (NumberFormatException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 }
