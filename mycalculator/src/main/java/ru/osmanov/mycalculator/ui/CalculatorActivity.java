8f9c33fef06dc02d06eb8d74e4d8f8d458b31f15
Nurulla Osmanov <Shivaii@yandex.ru>
2022-12-23 15:37:43 +0300

    добавил отображение ввода в TextView

--- mycalculator/src/main/java/ru/osmanov/mycalculator/ui/CalculatorActivity.java
+++ mycalculator/src/main/java/ru/osmanov/mycalculator/ui/CalculatorActivity.java
@@ -3,15 +3,25 @@ package ru.osmanov.mycalculator.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import ru.osmanov.mycalculator.R;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements CalculatorView{
+
+    private TextView resultView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
+        resultView = findViewById(R.id.tv_result);
+
+    }
+
+    @Override
+    public void showResult(String result) {
+        resultView.setText(result);
     }
 }
---
+++ mycalculator/src/main/java/ru/osmanov/mycalculator/ui/CalculatorView.java
@@ -0,0 +1,5 @@
+package ru.osmanov.mycalculator.ui;
+
+public interface CalculatorView {
+    void showResult (String result);
+}
